e8d0985cbdc92f8d71d15846d57ecffe423e6bf2
Emmanuel Adjei-Mensah <adjeimensah@gmail.com>
2024-04-24 22:34:44 +0000

    Day 1

---
+++ python_100_days/Day_1/project_band_generator.py
@@ -0,0 +1,13 @@
+Project Band name generator.
+#This project generates your band name by combining your city and names
+
+print("Welcome to the Band Name Generator.")
+
+city_name = input("Whats name of the city you grew up in?\n")
+pets_name = input("What's your pet's name?\n")
+
+band_name = (f"{city_name} {pets_name}")
+
+print(f"Your band name could be {band_name}.")
+
+
---
+++ python_100_days/README.md
@@ -0,0 +1,3 @@
+#Began Python 100 days challenge..
+Expecting it to be exciting.
+Project-based exercises only. using appbrewery python bootcamp resources
